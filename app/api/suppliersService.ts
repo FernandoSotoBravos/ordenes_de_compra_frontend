02e88a1137439878e93e07676262aa38bb8285a1
FernandoSotoBravos <fernando.soto@fcjuarez.com>
2025-10-22 11:46:21 -0600

    changes url to use the wrapper

--- app/api/suppliersService.ts
+++ app/api/suppliersService.ts
@@ -14,13 +14,14 @@ const exportExcel = async (token: string) => {
       responseType: "blob",
     })
     .then((response) => {
-      const blob = new Blob([response], {
+      const blob = new Blob([response.data], {
         type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
       });
       const url = window.URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
       a.download = "catalogo_proveedores.xlsx";
+      document.body.appendChild(a);
       a.click();
       a.remove();
       window.URL.revokeObjectURL(url);
@@ -30,6 +31,7 @@ const exportExcel = async (token: string) => {
       throw error;
     });
 };
+
 
 const getAll = async (token: string, limit: number = 10, page: number = 1) => {
   return fetchWrapper
